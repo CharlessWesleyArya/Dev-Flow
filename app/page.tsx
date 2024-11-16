874b7cb1cc8a74c023a2cfee0caa9d3c7ca67d99
Charless Wesley Arya <charlesswesleyarya@gmail.com>
2024-11-16 05:52:20 +0530

    Setup Light& Dark Theme using Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold">Welcome to Next Js</h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome to Spacegraytesk</h1>
     </div>
   );
 };
