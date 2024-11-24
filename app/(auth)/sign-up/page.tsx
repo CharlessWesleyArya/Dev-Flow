97bfb7ee19f018fd2ea74e2870ea352ab75b643a
Charless Wesley Arya <charlesswesleyarya@gmail.com>
2024-11-24 07:49:30 +0530

    Setup Auth Layouts,Social Auth Form, Next Auth, OAuth with Goolge and Github providers

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SigUp = () => {
+  return <div>Sign Up</div>;
+};
+
+export default SigUp;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <div>
+      <h1 className="h1-bold">Welcome to Next Js</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}>
+        <Button type="submit">Log out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
